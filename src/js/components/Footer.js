cabf55a7df91ab3dd6d720292f01e974dcf05784
Andrew Omelchenko <andrewom43@gmail.com>
2018-12-13 20:51:41 +0200

    fix footer's data-target id

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -13,10 +13,10 @@ class Footer extends Component {
     return `
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="#">WELCOME</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContentFooter" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContentFooter">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
               <a class="nav-link" href="https://www.facebook.com/profile.php?id=100013698678070">
