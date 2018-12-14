a8eec7e04ab1f0be3ab9873af3a47b944ec30d47
Andrew Omelchenko <andrewom43@gmail.com>
2018-12-14 08:10:10 +0200

    change icons in footer

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -13,7 +13,7 @@ class Footer extends Component {
     return `
       <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-dark">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContentFooter" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+          <i class="fa fa-expand" aria-hidden="true"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContentFooter">
           <ul class="navbar-nav mr-auto">
@@ -33,7 +33,9 @@ class Footer extends Component {
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="tel:+442074057686">+44(20) 74057686</a>
+              <a class="nav-link" href="tel:+442074057686">
+                <i class="fa fa-phone-square" aria-hidden="true"></i>
+              </a>
             </li>
           </ul>
         </div>
