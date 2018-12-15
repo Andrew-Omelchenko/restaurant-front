f9a8a897373ee284726a8d7f439eb716b9a697f5
Andrew Omelchenko <andrewom43@gmail.com>
2018-12-15 11:40:55 +0200

    add error handling to Events

--- src/js/components/Events.js
+++ src/js/components/Events.js
@@ -1,1 +1,53 @@
+import { AUTH_HTTP_SERVICE } from '../services/AuthHttpService';
+import { toHtml } from '../utils/helper';
+import Component from '../framework/Component';
+
+class Events extends Component {
+  constructor(props) {
+    super(props);
+
+    this.data = [];
+
+    this.host = document.createElement('main');
+    this.host.classList.add('container-fluid');
+
+    AUTH_HTTP_SERVICE.getEvents()
+      .then(res => {
+        console.log(res);
+        this.data = res.answer;
+        this.updateState({});
+      })
+      .catch(err => {
+        document
+            .getElementById('alert-placeholder')
+            .innerHTML = `Error status: ${err.status || 
+              'Server does not respond'}, ${err.answer || 
+              'check connection'}`;
+      });
+  }
+
+  render() {
+    let eventsStr = '';
+
+    this.data.forEach(element => {
+      eventsStr += ``;
+    });
+
+    const htmlString = `
+      <section>
+        <h3 class="text-sm-left text-center">Our Events</h3>
+        <p class="text-danger" id="alert-placeholder">&nbsp</p>
+        <div class="row text-center">
+          ${eventsStr}
+        </div>
+      </section>
+    `;
+
+    const node = toHtml(htmlString);
+
+    return node;
+  }
+}
+
+export default Events;
 
--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -18,9 +18,6 @@ class Navbar extends Component {
     
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#">ABOUT</span></a>
-            </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 MENU
@@ -33,13 +30,13 @@ class Navbar extends Component {
               </div>
             </li>
             <li class="nav-item">
+              <a class="nav-link" href="#/events">EVENTS</a>
+            </li>
+            <li class="nav-item">
               <a class="nav-link" href="#/reservations">RESERVATIONS</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#/location">LOCATE US</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">CONTACT</a>
             </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
