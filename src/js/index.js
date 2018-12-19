389331e5ea399f8b6fa8375aa4963372e5559190
Andrew Omelchenko <andrewom43@gmail.com>
2018-12-19 09:57:08 +0200

    add support for fetch in IE11

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import '../sass/main.scss';
 import 'bootstrap';
 import 'datatables.net-bs4';
 import '@webcomponents/webcomponentsjs';
+import 'whatwg-fetch';
 import Router from './framework/Router';
 import routes from './routes';
 
